7ea81d2ba182c2a99e0c55b1fd9b918bae0b6cc3
Hiral Patel <hiraldpatel15497@gmail.com>
2022-02-07 14:08:24 +0530

    Initial Commit

---
+++ client/build/precache-manifest.f3c6ec3006f407df501e7532cda3747e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f94e461440b7e0de3f418687c12a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe11c1ece0108427050",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe11c1ece0108427050",
+    "url": "/static/js/main.e67f03f0.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
